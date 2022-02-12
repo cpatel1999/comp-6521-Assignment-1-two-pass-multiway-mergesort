75a4aee8fb6860137f2ed53eb15686e21f7b772b
Charit_Patel <charit15499@gmail.com>
2022-02-12 18:31:53 -0500

    Phase 1 is implemented

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,63 +1,71 @@
+import java.util.LinkedList;
+
 public class QuickSort {
+
 
     /**
      * Swaps the elements in the provided positions.
      *
-     * @param array array to sort
-     * @param first first index
+     * @param list   list to sort
+     * @param first  first index
      * @param second second index
      */
-    public void swap(int[] array, int first, int second) {
-        int temporary = array[first];
-        array[first] = array[second];
-        array[second] = temporary;
+    public void swap(LinkedList<Integer> list, int first, int second) {
+        int temporary = list.get(first);
+        list.add(first, list.get(second));
+        list.add(second, temporary);
     }
 
     /**
      * This function takes last element as pivot, places the pivot element at its correct position in sorted array,
      * and places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot
      *
-     * @param array array to sort
-     * @param low first index
+     * @param list list to sort
+     * @param low  first index
      * @param high last index
      * @return pivot element index
      */
-    public int partitionArray(int[] array, int low, int high) {
+    public int partitionArray(LinkedList<Integer> list, int low, int high) {
 
         // pivot
-        int p = array[high];
+        int p = list.get(high);
         int j = (low - 1);
 
         for (int i = low; i <= high - 1; i++) {
 
             // Check if current element is less than the pivot element
-            if (array[i] < p) {
+            if (list.get(i) < p) {
                 // If current element is smaller than the pivot element then
                 // Increment index of smaller element
                 j++;
-                swap(array, j, i);
+                swap(list, j, i);
             }
         }
-        swap(array, j + 1, high);
+        swap(list, j + 1, high);
         return (j + 1);
     }
 
     /**
      * Function that implements QuickSort.
      *
-     * @param array array to sort
-     * @param low first index
+     * @param list list to sort
+     * @param low  first index
      * @param high last index
      */
-     public void quickSort(int[] array, int low, int high) {
+    public void quickSort(LinkedList<Integer> list, int low, int high) {
         if (low < high) {
 
             // pivot is partitioning index, arr[p] is now at right place
-            int pivot = partitionArray(array, low, high);
+            int pivot = partitionArray(list, low, high);
 
             // Separately sort elements before partition and after partition
-            quickSort(array, low, pivot - 1);
-            quickSort(array, pivot + 1, high);
+            quickSort(list, low, pivot - 1);
+            quickSort(list, pivot + 1, high);
         }
+    }
+
+    public LinkedList<Integer> sort(LinkedList<Integer> list) {
+        quickSort(list, 0, list.size() - 1);
+        return list;
     }
 }
--- src/TwoPhaseMultiWayMergeSort.java
+++ src/TwoPhaseMultiWayMergeSort.java
@@ -1,31 +1,75 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class TwoPhaseMultiWayMergeSort {
 
-    public LinkedList<Integer> numberList = new LinkedList<>();
-    QuickSort quickSort = new QuickSort();
-    public void twoPhaseSort(int ramSize) {
-        try (Scanner fileReader = new Scanner(new FileReader("docs/file1.txt"))) {
-            while (fileReader.hasNextLine()) {
-                int number = Integer.parseInt(fileReader.nextLine().trim());
-                numberList.add(number);
-            }
+    public QuickSort quickSort = new QuickSort();
+    public String path = "docs/";
+    public ArrayList<String> filesList = new ArrayList<>();
 
-            phase_1(ramSize);
-        }catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
+    public void twoPhaseSort(int ramSize) {
+        phase_1(ramSize);
     }
 
     public void phase_1(int ramSize) {
-        int currentBlockCount = 0;
-        int numbersCount = numberList.size();
-        int blockCount = (int) Math.ceil((double)numbersCount / (double)ramSize);
-        System.out.println("Blocks count: " + blockCount);
 
+        boolean run = true;
+        int recordCount = 0;
+        int dataCount = 0;
+        int currentBlock = 1;
+        try (Scanner fileReader = new Scanner(new FileReader("docs/file1.txt"))) {
+            while (run) {
 
+                LinkedList<Integer> numberList = new LinkedList<>();
+                while (fileReader.hasNextLine()) {
+
+                    int number = Integer.parseInt(fileReader.nextLine().trim());
+                    numberList.add(number);
+                    recordCount++;
+                    ++dataCount;
+                    if (dataCount == ramSize) {
+                        dataCount = 0;
+                        break;
+                    }
+                }
+                numberList = quickSort.sort(numberList);
+
+                String outputFile = path + "block-" + currentBlock;
+                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+                for (int i = 0; i < numberList.size(); i++) {
+                    writer.write(numberList.get(i) + "\n");
+                }
+                writer.close();
+
+                filesList.add(outputFile);
+
+                if (!fileReader.hasNextLine()) {
+                    break;
+                }
+                currentBlock++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("IO Error!");
+            System.exit(0);
+        }
     }
 }
+
+
+//        int currentBlockCount = 0;
+//        int numbersCount = numberList.size();
+//        int blockCount = (int) Math.ceil((double)numbersCount / (double)ramSize);
+//        System.out.println("Blocks count: " + blockCount);
+//        int[] singleBlock = new int[ramSize];
+//
+//        for(int i = 0; i < numbersCount; i++) {
+//            if(currentBlockCount < blockCount) {
+//                singleBlock
+//            }
+//        }
