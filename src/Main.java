82b89eb51d56fce3cd950dcdcc9c30e43b633e9f
Charit_Patel <charit15499@gmail.com>
2022-02-12 15:14:42 -0500

    Random numbers are generated and written in the text file

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the count for random number:");
+        int count = sc.nextInt();
+
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        randomNumberGenerator.generate(count);
     }
 }
---
+++ src/RandomNumberGenerator.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * Generator class to generate the random numbers.
+ */
+public class RandomNumberGenerator {
+
+    /**
+     * Generates the given numbers of random numbers in specific range.
+     *
+     * @param count count to generate random numbers.
+     */
+    public void generate(int count) {
+        FileWriter writer = null;
+        BufferedWriter bufferedWriter = null;
+        int randomNumber;
+        try {
+            writer = new FileWriter("docs/file1.txt");
+            bufferedWriter = new BufferedWriter(writer);
+            Random random = new Random();
+            for(int i = 0; i < count; i++) {
+                randomNumber = random.nextInt(0, count * 5);
+                bufferedWriter.write(randomNumber + "\n");
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
